c47610ead4133a4bf9558067297bf4e2aaeca95d
unknown <anonimus.anonim.2002@list.ru>
2023-07-01 19:59:05 +0300

    Little changes in AGS into Vivado

---
+++ Vivado/EMA/src/EMA_Module.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//(* use_dsp = "yes" *)
+module EMA_Module
+#(   
+    parameter BWIDTH   = 18,
+    parameter AWIDTH   = 30,
+    parameter DWIDTH   = 27,
+//    parameter MULT = 45,
+    parameter OUTWIDTH = 48  //parameter
+) (
+    input  wire clk,
+    input  wire [BWIDTH-1:0     ] Filter_Coefficient,
+    input  wire [DWIDTH-1:0     ] Port_Data,
+    input  wire                   Valid,
+    output wire                   Valid_out_ema,
+    output wire [OUTWIDTH-1:0   ] Filter_Out
+);
+
+//reg signed [AWIDTH-1:0]   a;
+reg signed [DWIDTH-1:0]   Data;
+reg signed [BWIDTH-1:0]   Coeff_1,Coeff_2;
+reg signed [DWIDTH-1:0]   Pread; 
+//reg signed [MULT-1:0]     mult;
+reg signed [OUTWIDTH-1:0] c,accum ;
+reg Valid_1,Valid_2,Valid_3;
+
+//INMODE <= "11101";
+
+always @(posedge clk) begin
+//    a1 <= accum;
+    Valid_1 <= Valid;
+    if (Valid) begin
+        Data    <= Port_Data;
+    end
+    Coeff_1 <= Filter_Coefficient;
+    Valid_2 <= Valid_1;
+    Pread   <= Data  - accum;
+    Coeff_2 <= Coeff_1;
+//    mult     = Coeff * Pread;
+    if (Valid_2) begin
+        accum   <= Coeff_2 * Pread + c;
+        Valid_3 <= Valid_2;
+    end
+
+    if (Valid_3) begin
+        c     <= accum;
+    end
+
+end
+
+assign Filter_Out = accum;
+assign Valid_out_ema  = Valid_2;
+
+endmodule
--- Vivado/EMA/src/Test1.v
+++ Vivado/EMA/src/Test1.v
@@ -1,27 +1,31 @@
 `timescale 1ns / 1ps
 
-
-
 module Test1
-	#(   
+    #(   
     parameter W_IN = 16,
     parameter W_IN_MODULE = 26,
     parameter BWIDTH = 18,
-    parameter W_OUT = 48
+    parameter AWIDTH = 30,
+    parameter DWIDTH = 27,
+    parameter W_OUT = 16
 ) (
     input  wire clk,
     input  wire [W_IN -1:0 ] s_chans_dataI,
     input  wire [W_IN -1:0 ] s_chans_dataQ,
-    input  wire 			 s_chans_valid,
+    input  wire              s_chans_valid,
     input  wire [BWIDTH-1:0] Filter_Coefficient,
-    output wire 			 Valid_Out,
-    output wire [W_OUT-1:0 ] Output
+    input  wire [BWIDTH-1:0] Error_Coefficient,
+    input  wire [DWIDTH-1:0] R_level,
+    output wire              Valid_Out,
+    output wire [W_OUT-1:0 ] OutputI,
+    output wire [W_OUT-1:0 ] OutputQ
 );
 
 
 wire [W_IN_MODULE:0] connection;
-
-wire valid_connection;
+wire [AWIDTH-1:0   ] error_connect;
+wire [W_OUT-1:0    ] error_last;  
+wire valid_connection,valid_error,valid_out_error;
 
 reg   [0:0]  Valid_1,Valid_2;
 
@@ -29,7 +33,15 @@ reg   [W_IN-1:0] received_Isignal,received_Qsignal;
 
 reg   [W_IN_MODULE-1:0] first_Imult,first_Qmult;
 
-reg [10-1:0] out_chance = 1'b0000000001;
+wire  [W_IN-1:0] Satureted_real,Satureted_imag;
+
+reg   [W_OUT-1:0] I_out_reg,Q_out_reg;
+
+    
+    assign Valid_Out  = valid_out_error;
+    assign OutputQ    = Q_out_reg;
+    assign OutputI    = I_out_reg;
+
 
 always @(posedge clk) begin
 
@@ -38,11 +50,37 @@ always @(posedge clk) begin
         received_Qsignal <= s_chans_dataQ;
     end
 
-    first_Imult <= received_Isignal * out_chance;
-    first_Qmult <= received_Qsignal * out_chance;
+
+
+    first_Imult <= received_Isignal * error_last;
+    first_Qmult <= received_Qsignal * error_last;
     Valid_1     <= s_chans_valid;
     Valid_2     <= Valid_1;
+
+    if (Valid_Out)begin
+        Q_out_reg     = Satureted_real;
+        I_out_reg     = Satureted_imag;
+    end
  end
+    Overflow_Satureted #(
+        .W_IN(W_IN_MODULE), 
+        .W_OUT(W_IN)
+    ) 
+    inst_Overflow_Satureted_real 
+    (
+        .Data_In(first_Qmult),
+        .Saturated_Out(Satureted_real)
+    );
+
+        Overflow_Satureted #(
+        .W_IN(W_IN_MODULE), 
+        .W_OUT(W_IN)
+    ) 
+    inst_Overflow_Satureted_imag 
+    (
+        .Data_In(first_Imult),
+        .Saturated_Out(Satureted_imag)
+    );
 
 
     Module_of_number #(
@@ -54,17 +92,32 @@ always @(posedge clk) begin
         .Output    (connection)
     );
 
-	EMA_Module #(
-		.BWIDTH(BWIDTH),
-		.DWIDTH(W_IN_MODULE+1),
-		.OUTWIDTH(W_OUT)
-	) inst_EMA_Module (
-		.clk  (clk),
-		.Filter_Coefficient (Filter_Coefficient),
-		.Port_Data (connection) ,
-		.Valid (Valid_2),
-		.Valid_out_ema (Valid_Out),
-		.Filter_Out (Output)
-	);
+    EMA_Module #(
+        .BWIDTH(BWIDTH),
+        .DWIDTH(DWIDTH),
+        .OUTWIDTH(W_OUT)
+    ) inst_EMA_Module (
+        .clk  (clk),
+        .Filter_Coefficient (Filter_Coefficient),
+        .Port_Data (connection) ,
+        .Valid (Valid_2),
+        .Valid_out_ema (valid_error),
+        .Filter_Out (error_connect)
+    );
+
+    Error #(
+        .BWIDTH(BWIDTH),
+        .AWIDTH(AWIDTH),
+        .DWIDTH(DWIDTH),
+        .OUTWIDTH(W_OUT)
+    ) inst_Error (
+        .clk               (clk),
+        .Error_Coefficient (Error_Coefficient),
+        .Port_Data_A       (error_connect),
+        .R_level           (R_level),
+        .Valid             (valid_error),
+        .Valid_out_error   (valid_out_error),
+        .Error_Out         (error_last)
+    );
 
 endmodule
